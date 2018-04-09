0f57a0bdfec5c686dc257c57132efa0baefe5aed
panossot <psotirop@redhat.com>
2018-04-09 14:45:55 +0200

    Preparing for release 1.0.8.Final.

--- eap-additional-testsuite-source-distribution/src/main/java/org/jboss/eap/additional/testsuite/source/distribution/ProcessSourcesEatPm.java
+++ eap-additional-testsuite-source-distribution/src/main/java/org/jboss/eap/additional/testsuite/source/distribution/ProcessSourcesEatPm.java
@@ -172,6 +172,8 @@ public class ProcessSourcesEatPm {
                         }
                     }
                 }
+                if (result)
+                    break;
             }
             
             if(!result)
